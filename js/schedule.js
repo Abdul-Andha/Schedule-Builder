341fc280902e8b96a38bf9387580bafee0dce4cf
abdul-andha <aandha8342@gmail.com>
2022-02-26 15:08:54 -0500

    adding functionality

--- js/schedule.js
+++ js/schedule.js
@@ -1,65 +1,71 @@
 //test data
-let tasks = [
-  {
-    description: 'task #1',
-    hours: 2,
-    scheduledHrs: 0
-  },
-  {
-    description: 'task #2',
-    hours: 4,
-    scheduledHrs: 0
-  },
-  {
-    description: 'task #3',
-    hours: 6.5,
-    scheduledHrs: 0
-  },
-  {
-    description: 'task #4',
-    hours: 0.5,
-    scheduledHrs: 0
-  },
-  {
-    description: 'task #5',
-    hours: 12,
-    scheduledHrs: 0
-  },
-  {
-    description: 'task #6',
-    hours: 0.22,
-    scheduledHrs: 0
-  },
-  {
-    description: 'task #7',
-    hours: 0.1,
-    scheduledHrs: 0
-  },
-];
-let avail = [3, 7, 2, 3, 5, 6, 4];
+// let tasks = [
+//   {
+//     description: 'task #1',
+//     minutes: 2,
+//     scheduledMins: 0
+//   },
+//   {
+//     description: 'task #2',
+//     minutes: 4,
+//     scheduledMins: 0
+//   },
+//   {
+//     description: 'task #3',
+//     minutes: 6.5,
+//     scheduledMins: 0
+//   },
+//   {
+//     description: 'task #4',
+//     minutes: 0.5,
+//     scheduledMins: 0
+//   },
+//   {
+//     description: 'task #5',
+//     minutes: 12,
+//     scheduledMins: 0
+//   },
+//   {
+//     description: 'task #6',
+//     minutes: 0.22,
+//     scheduledMins: 0
+//   },
+//   {
+//     description: 'task #7',
+//     minutes: 0.1,
+//     scheduledMins: 0
+//   },
+// ];
+let avail;
 let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 function main() {
   //early return
+  let errorMsg = document.getElementById('error-msg');
+  avail = getAvail();
   let totalAvail = avail.reduce((a, b) => a + b, 0);
-  let totalHours = getTotalHours();
-  if (totalAvail < totalHours) {
-    console.log("Error: You dont have enough availability to generate a schedule. Reduce a task or increase your availability.");
+  let totalMins = getTotalMins();
+
+  if (tasks.length == 0) {
+    errorMsg.innerHTML = "Please enter a task."
+    return;
+  }
+  if (totalAvail < totalMins) {
+    errorMsg.innerHTML = "You dont have enough availability to generate a schedule. Please increase your availability.";
     return;
   }  
 
-  let week = getWeek(totalHours, totalAvail);
-  console.log(week);
+  let week = getWeek(totalMins, totalAvail);
   getSchedule(week);
 }
 
-function getWeek(totalHours, totalAvail) {
-  let workPerHour = totalHours / totalAvail;
+function getWeek(totalMins, totalAvail) {
+  let workPerMin = totalMins / totalAvail;
   let week = [];
   for (let i = 0; i < days.length; i++) {
     let weekEntry = {
       name: days[i],
-      workHours: avail[i] * workPerHour
+      workMins: avail[i] * workPerMin
     }
     week.push(weekEntry);
   }
@@ -70,17 +76,17 @@ function getSchedule(week) {
   let schedule = "";
   week.forEach(day => {
     schedule += day.name + "\n";
-    let leftOverTime = day.workHours;
+    let leftOverTime = day.workMins;
     while (leftOverTime > 0) {
-      let targetTask = tasks.find(task => task.scheduledHrs < task.hours);
-      let unscheduledHrs = targetTask.hours - targetTask.scheduledHrs;
+      let targetTask = tasks.find(task => task.scheduledMins < task.minutes);
+      let unscheduledMins = targetTask.minutes - targetTask.scheduledMins;
 
-      if (leftOverTime >= unscheduledHrs) {
-        targetTask.scheduledHrs += unscheduledHrs;
-        leftOverTime -= unscheduledHrs;
-        schedule += targetTask.description + ` for ${formatTime(unscheduledHrs)}\n`;
+      if (leftOverTime >= unscheduledMins) {
+        targetTask.scheduledMins += unscheduledMins;
+        leftOverTime -= unscheduledMins;
+        schedule += targetTask.description + ` for ${formatTime(unscheduledMins)}\n`;
       } else {
-        targetTask.scheduledHrs += leftOverTime;
+        targetTask.scheduledMins += leftOverTime;
         schedule += targetTask.description + ` for ${formatTime(leftOverTime)}\n`;
         leftOverTime = 0;
       }
@@ -88,26 +94,23 @@ function getSchedule(week) {
     schedule += "\n";
   })
   console.log(schedule);
-  console.log(tasks);
 }
 
-function getTotalHours() {
+function getTotalMins() {
   let total = 0;
   tasks.forEach((task) => {
-    total += task.hours
+    total += task.minutes
   })
   return total;
 }
 
-function formatTime(hours) {
-  let finalHours = Math.floor(hours)
-  let minutes = Math.round((hours - finalHours) * 60);
+function formatTime(mins) {
+  let hours = Math.floor(mins / 60);
+  let minutes = Math.round(mins - (hours * 60));
   let returnStr = "";
 
-  if (finalHours > 0) returnStr += finalHours + " hours";
+  if (hours > 0) returnStr += hours + " hours";
   if (minutes > 0)returnStr += " " + minutes + " minutes";
 
   return returnStr;
 }
-
-main();
---
+++ js/script.js
@@ -0,0 +1,79 @@
+let tasks = [];
+
+let enter_task = () => {
+    const taskDesc = document.getElementById("task").value;
+    const hours = document.getElementById("hours").value;
+    const minutes = document.getElementById("minutes").value;
+    
+    let taskEntry = {
+        description: taskDesc,
+        minutes: (hours * 60) + parseInt(minutes),
+        scheduledMins: 0
+    }
+    
+    tasks.push(taskEntry);
+    document.getElementById("task-form").reset;
+    
+    // if (day == "Sunday") {
+    //     let ele = document.getElementById("sunday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else if (day == "Monday") {
+    //     let ele = document.getElementById("monday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else if (day == "Tuesday") {
+    //     let ele = document.getElementById("tuesday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else if (day == "Wednesday") {
+    //     let ele = document.getElementById("wednesday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else if (day == "Thursday") {
+    //     let ele = document.getElementById("thursday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else if (day == "Friday") {
+    //     let ele = document.getElementById("friday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else if (day == "Saturday") {
+    //     let ele = document.getElementById("saturday");
+    //     ele.innerHTML += task + " for " + hour + " and " + minute + "<br>";
+    // }
+    // else {
+    //     // pass
+    // }
+};
+
+function getAvail() {
+    let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    let avail = [];
+    days.forEach(day => {
+        let dailyVal = document.getElementById(day + '-hrs').value;
+        avail.push(dailyVal * 60);
+    });
+    return avail;
+}
+
+$(function(){
+    $(":submit").click(function(){
+        var elmt=document.getElementById('form');
+        domtoimage.toPng(elmt)
+        .then(function(dataUrl){
+            var img=new Image();
+            img.src=dataUrl;
+            document.body.appendChild(img);
+        })
+        .catch(function(error){
+            console.error('error function', error);
+        })
+    });
+});
+
+function displayRange(day) {
+    let value = document.getElementById(day + '-hrs').value;
+    let label = document.getElementById(day + '-lbl');
+    label.innerHTML = value + " hours";
+}
