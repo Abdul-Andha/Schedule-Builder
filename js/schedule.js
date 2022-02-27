435d29f5d3affe1e41ba1ceadcbb60978f3282e2
abdul-andha <aandha8342@gmail.com>
2022-02-26 20:25:01 -0500

    fixed schedule display

--- js/schedule.js
+++ js/schedule.js
@@ -1,4 +1,8 @@
 //global variables
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 let avail;
 let tasks = [];
 let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -9,6 +13,7 @@ function main() {
   document.getElementById('schedule-list').innerHTML = "";
   let errorMsg = document.getElementById('error-msg2');
   errorMsg.innerHTML = "";
+<<<<<<< Updated upstream
 
   //early return
   avail = getAvail();
@@ -24,22 +29,39 @@ function main() {
   }  
   
   //make and display schedule
+=======
+
+  //early return
+  avail = getAvail();
+  let totalAvail = avail.reduce((a, b) => a + b, 0);
+  
+  if (tasks.length == 0) {
+    errorMsg.innerHTML = "There are no tasks. Please enter a task."
+    return;
+  }
+  let totalMins = getTotalMins();
+  if (totalAvail < totalMins) {
+    errorMsg.innerHTML = "You dont have enough availability to generate a schedule. Please increase your availability.";
+    return;
+  }  
+
+>>>>>>> Stashed changes
   let week = getWeek(totalMins, totalAvail);
   let schedule = getSchedule(week);
   displaySchedule(schedule);
 }
 
 function getWeek(totalMins, totalAvail) {
-    let workPerMin = totalMins / totalAvail;
-    let week = [];
-    for (let i = 0; i < days.length; i++) {
-        let weekEntry = {
-        name: days[i],
-        workMins: avail[i] * workPerMin
-        }
-        week.push(weekEntry);
+  let workPerMin = totalMins / totalAvail;
+  let week = [];
+  for (let i = 0; i < days.length; i++) {
+    let weekEntry = {
+      name: days[i],
+      workMins: avail[i] * workPerMin
     }
-    return week;
+    week.push(weekEntry);
+  }
+  return week;
 }
 
 function getSchedule(week) {
@@ -79,20 +101,20 @@ function displaySchedule(schedule) {
 }
 
 function getTotalMins() {
-    let total = 0;
-    tasks.forEach((task) => {
-        total += task.minutes
-    })
-    return total;
+  let total = 0;
+  tasks.forEach((task) => {
+    total += task.minutes
+  })
+  return total;
 }
 
 function formatTime(mins) {
-    let hours = Math.floor(mins / 60);
-    let minutes = Math.round(mins - (hours * 60));
-    let returnStr = "";
+  let hours = Math.floor(mins / 60);
+  let minutes = Math.round(mins - (hours * 60));
+  let returnStr = "";
 
-    if (hours > 0) returnStr += hours + " hours";
-    if (minutes > 0)returnStr += " " + minutes + " minutes";
+  if (hours > 0) returnStr += hours + " hours";
+  if (minutes > 0)returnStr += " " + minutes + " minutes";
 
-    return returnStr;
+  return returnStr;
 }
